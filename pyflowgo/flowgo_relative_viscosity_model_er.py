9b985facd2d73aed18529773b9a0de2e79ad90b6
Jérémie Labroquère <jeremie.labroquere@gmail.com>
2017-06-07 00:29:56 +0200

    Add licence in source files.

--- pyflowgo/flowgo_relative_viscosity_model_er.py
+++ pyflowgo/flowgo_relative_viscosity_model_er.py
@@ -1,3 +1,19 @@
+# Copyright 2017 PyFlowGo development team (Oryaelle Magdalena Chevrel and Jeremie Labroquere)
+#
+# This file is part of the PyFlowGo library.
+#
+# The PyFlowGo library is free software: you can redistribute it and/or modify
+# it under the terms of the the GNU Lesser General Public License as published by 
+# the Free Software Foundation; either version 3 of the License, or 
+# (at your option) any later version.
+#
+# The PyFlowGo library is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU General Public License for more details.
+#
+# You should have received copies of the GNU Lesser General Public License 
+# along with the PyFlowGo library.  If not, see https://www.gnu.org/licenses/.
 import math
 import json
 
