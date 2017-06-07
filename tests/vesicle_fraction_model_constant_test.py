79ae8c0eeab7bfd507162e2cd58aff641b39dbe6
Jérémie Labroquère <jeremie.labroquere@gmail.com>
2017-06-07 22:41:18 +0200

    Updqting license in python files.

--- tests/vesicle_fraction_model_constant_test.py
+++ tests/vesicle_fraction_model_constant_test.py
@@ -1,19 +1,19 @@
-# Copyright 2017 PyFlowGo development team (Oryaelle Magdalena Chevrel and Jeremie Labroquere)
+# Copyright 2017 PyFLOWGO development team (Magdalena Oryaelle Chevrel and Jeremie Labroquere)
 #
-# This file is part of the PyFlowGo library.
+# This file is part of the PyFLOWGO library.
 #
-# The PyFlowGo library is free software: you can redistribute it and/or modify
-# it under the terms of the the GNU Lesser General Public License as published by 
-# the Free Software Foundation; either version 3 of the License, or 
+# The PyFLOWGO library is free software: you can redistribute it and/or modify
+# it under the terms of the the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
-# The PyFlowGo library is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# The PyFLOWGO library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received copies of the GNU Lesser General Public License 
-# along with the PyFlowGo library.  If not, see https://www.gnu.org/licenses/.
+# You should have received copies of the GNU Lesser General Public License
+# along with the PyFLOWGO library.  If not, see https://www.gnu.org/licenses/.
 
 import unittest
 import pyflowgo.flowgo_vesicle_fraction_model_constant
