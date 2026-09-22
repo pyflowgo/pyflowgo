dff38dda117c33a107762e4ef74a715277e35fd9
Orya <oryaelle.chevrel@gmail.com>
2026-09-22 15:04:36 +0200

    modify the way the effusion rate is transmitted to the file runout. Now it take teh name of the file instead of the calculated effusion rate that may not be exactly the same

--- pyflowgo/run_outs.py
+++ pyflowgo/run_outs.py
@@ -43,8 +43,8 @@ def get_run_outs(path_to_folder, flowgo_results, slope_file, lava_name):
                     yieldstrength_array.append(float(row['tho_0']))
 
             # Initial effusion rate
-            effusion_rate_init_calc = round(effusion_rate[0], 0)   # m3/s
-            print("effusion_rate_init_calculated = ", effusion_rate_init_calculated)
+            #effusion_rate_init_calc = round(effusion_rate[0], 0)   # m3/s
+            #print("effusion_rate_init_calculated = ", effusion_rate_init_calculated)
 
             filename_base = os.path.basename(filename)
             effusion_rate_init = float(filename_base.rsplit("_", 1)[1].replace("m3s.csv", ""))
